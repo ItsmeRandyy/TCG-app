d85e1c1c323bf5e60d5a4661ac411b9c378e3f3c
ItsmeRandyy <139468401+ItsmeRandyy@users.noreply.github.com>
2025-07-14 11:43:51 +0200

    trying more to get images

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
 import {useEffect, useState} from 'react'
+import tropius from './assets/cards/sv1a-tropius.jpg'
+import sprigatito from './assets/cards/sv1a-sprigatito.jpg'
+import floragato from './assets/cards/sv1a-floragato.jpg'
+import meowscarada from './assets/cards/sv1a-meowscarada.jpg'
+import fuecoco from './assets/cards/sv1a-fuecoco.jpg'
+import pwooper from './assets/cards/sv1a-pwooper.jpg'
+import fidough from './assets/cards/sv1s-fidough.jpg'
+import ralts from './assets/cards/sv1s-ralts.jpg'
+import kirlia from './assets/cards/sv1s-kirlia.jpg'
+import gardevoir from './assets/cards/sv1s-gardevoir.jpg'
 
 
 const Card = ({cardImg, cardNumber, collectionName, Pokemon}) => {
@@ -41,28 +51,26 @@ return (
 </div>
 )
 }
-const images = import.meta.glob('./assets/cards/*.jpg', { eager: true, import: 'default' })
 
 const App = () => {
-  return (
-    <>
-      <h2>TCG Wishlist</h2>
-      <div className="card-row">
-        {cards.map(card => {
-          const cardImg = images[`./assets/cards/${card.file}`]
-          return (
-            <Card
-              key={card.name}
-              cardImg={cardImg}
-              collectionName={card.collection}
-              cardNumber={card.number}
-              Pokemon={card.name}
-            />
-          )
-        })}
-      </div>
-    </>
-  )
+
+return (
+  <>
+    <h2>TCG Wishlist</h2>
+    <div className="card-row">
+<Card cardImg={tropius} collectionName="sv1a" cardNumber="074/073" Pokemon="Tropius"/>
+<Card cardImg={sprigatito} collectionName="sv1a" cardNumber="075/073" Pokemon="Sprigatito"/>
+<Card cardImg={floragato} collectionName="sv1a" cardNumber="076/073" Pokemon="Floragato"/>
+<Card cardImg={meowscarada} collectionName="sv1a" cardNumber="077/073" Pokemon="Meoscarada"/>
+<Card cardImg={fuecoco} collectionName="sv1a" cardNumber="078/073" Pokemon="Fuecoco"/>
+<Card cardImg={pwooper} collectionName="sv1a" cardNumber="076/073" Pokemon="P. Wooper"/>
+<Card cardImg={fidough} collectionName="sv1S" cardNumber="085/073" Pokemon="Fidough"/> 
+<Card cardImg={ralts} collectionName="sv1S" cardNumber="083/073" Pokemon="Ralts"/>
+<Card cardImg={kirlia} collectionName="sv1S" cardNumber="084/073" Pokemon="Kirlia"/>
+<Card cardImg={gardevoir} collectionName="sv1S" cardNumber="101/073" Pokemon="Gardevoir "/>
+    </div>
+  </>
+)
 }
 
 export default App
